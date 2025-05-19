9fdfb65c9c3c0fdaff6c2144a1bdf6531c710d36
SidNeilK <siddharthneilk@gmail.com>
2025-05-19 17:27:04 -0500

    Sum Proudct

--- SumProduct.py
+++ SumProduct.py
@@ -3,15 +3,15 @@ def calculateSum(numbers):
         return 0
     total=0
     for i in range(len(numbers)):
-        total+=numbers
+        total+=numbers[i]
     return total
 
 def calculateProduct(numbers):
     if len(numbers)==0:
         return 1
-    total=0
+    total=1
     for i in range(len(numbers)):
-        total*=numbers
+        total*=numbers[i]
     return total
 
 assert calculateSum([]) == 0
